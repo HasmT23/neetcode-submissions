3c1d2d5f5adabc52bb1fb3f71b4b3c7158335f06
HasmT23 <150742541+HasmT23@users.noreply.github.com>
2026-07-06 10:27:06 -0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashseen = set()
+
+        for n in nums:
+            if n in hashseen:
+                return True
+            hashseen.add(n)
+        return False
+
+            
+            
+            
